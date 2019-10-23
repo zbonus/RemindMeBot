742ecb995ad609c892302344a86d5494063c0c00
sjbollno <43500323+sjbollno@users.noreply.github.com>
2019-10-23 16:34:12 -0400

    added minute timer

--- RemindMeDiscordBot/Commands/timerm.js
+++ RemindMeDiscordBot/Commands/timerm.js
@@ -1,8 +1,8 @@
 module.exports = {
-    name: 'timers',
-    description: 'pings user after a certain time interval in seconds.',
+    name: 'timerm',
+    description: 'pings user after a certain time interval in minutes.',
     args: true,
-    usage: '<time interval (seconds)>',
+    usage: '<time interval (minutes)>',
     execute(client, message, args) {
         if (isNaN(args[0]))
         {
@@ -10,7 +10,7 @@ module.exports = {
         }
         else
         {
-            msecs = args[0] * 1000;
+            msecs = args[0] * 1000 * 60;
             setTimeout(timerdone, `${msecs}`, client,  message, args[0]);
         }
     },
@@ -20,10 +20,10 @@ function timerdone(client , message, arg)
 {
     if (arg == 1)
     {
-        message.reply('1 second has passed');
+        message.reply('1 minute has passed');
     }
     else
     {
-        message.reply(`${arg} seconds have passed.`);
+        message.reply(`${arg} minutes have passed.`);
     }
 }
