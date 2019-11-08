9fbd38c90bdce9d3631293a56f9b1fb6b0aa8e32
MTUCoder <43008988+MTUCoder@users.noreply.github.com>
2019-11-08 13:46:37 -0500

    Modified bot to ping database for reminders that will occur within the next five minutes

--- RemindMeDiscordBot/Commands/dbtest.js
+++ RemindMeDiscordBot/Commands/dbtest.js
@@ -1,12 +1,6 @@
 module.exports = {
     name: 'dbtest',
     execute(client, message, args, dbConn) {
-        function printQueryResults(sql, results) {
-            console.log(sql);
-            results.forEach(function (item) {
-                console.log(`${item.user_id} ${item.react_id} ${item.message}`);
-            });
-        }
 
         var test1passed = false;
         //TODO: insert call to !remindme 12/20/2019 23:59 "End of fall semester"
@@ -14,16 +8,16 @@ module.exports = {
         dbConn.query(sql, function (error, results, fields) {
             if (results.length == 1 && !error) {
                 message.channel.send("Database Test 1: Passed");
-                printQueryResults(sql, results);
+                dbConn.printQueryResults(sql, results);
                 test1passed = true;
             } else if (error) {
                 console.log(error);
                 message.channel.send("Database Test 1: Failed. General database error");
-                printQueryResults(sql, results);
+                dbConn.printQueryResults(sql, results);
                 return;
             } else {
                 message.channel.send(`Database Test 1: Failed. Expected only 1 reminder; instead got ${results.length}.`);
-                printQueryResults(sql, results);
+                dbConn.printQueryResults(sql, results);
             }
         });
 
