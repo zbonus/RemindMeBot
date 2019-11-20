54ef80abb2c93f1468fc55cf81d0dbc7dd153701
MTUCoder <43008988+MTUCoder@users.noreply.github.com>
2019-11-19 22:21:21 -0500

    Added ability for user to delete all of their reminders

--- RemindMeDiscordBot/Commands/deletereminder.js
+++ RemindMeDiscordBot/Commands/deletereminder.js
@@ -2,15 +2,36 @@ module.exports = {
     name: 'deletereminder',
     description: 'Allows a user to delete one of their stored reminders',
     args: true,
-    usage: '<react_id>',
+    usage: '<react_id> <flag>',
     execute(client, message, args, dbConn) {
-        dbConn.query(`SELECT user_id FROM id WHERE react_id = ${args[0]}`, function (error, results) {  // get user id of the given react_id
-            if (error) {
-                console.log(error);
-                message.channel.send("Internal Database Error. Please check your arguemnts, or report the problem to the developers.");
+        function dbError(error) {
+            console.log(error);
+            message.channel.send("Internal Database Error. Please report the problem to the developers.");
+        }
+
+        if (args[1]) {
+            if (args[1] == "!") {
+                dbConn.query(`DELETE FROM multiple WHERE user_id = ${message.author.id};` + 
+                             `DELETE FROM single WHERE user_id = ${message.author.id};` +
+                             `DELETE FROM id WHERE user_id = ${message.author.id};`, function (error, results) {
+                    if (error) {
+                        dbError(error);
+                        return;
+                    }
+                    message.reply("your reminders have been deleted");
+                });
                 return;
             }
-            if (uid !== message.author.id) {    // if the react_id is linked to a different user, do not let them know
+            message.channel.send("Heads up! Inserting a '!' in for the second argument of this command will delete all of your reminders. Use with caution!");
+            return;
+        }
+
+        dbConn.query(`SELECT user_id FROM id WHERE react_id = ${args[0]}`, function (error, results) {  // get user id of the given react_id
+            if (error) {
+                dbError(error);
+                return;
+            }
+            if (!results[0]) {  // if the reminder is not found
                 message.channel.send("A reminder with this ID does not exist");
                 return;
             }
@@ -18,6 +39,10 @@ module.exports = {
         });
 
         function a(rid, uid) {
+            if (uid !== message.author.id) {    // if the react_id is linked to a different user, do not let them know
+                message.channel.send("A reminder with this ID does not exist");
+                return;
+            }
             dbConn.query("SELECT * FROM (" +    // find the table that the reminder resides in
                             "SELECT * FROM (" +
                                 "SELECT react_id, user_id, 'single' AS table_name FROM single" +
